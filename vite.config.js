ebe2ce266c822187859366550affeb1aa51edff2
Avukus <akashdeeprana04@gmail.com>
2025-12-19 18:52:46 +0530

    Final attempt: using relative base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Movie-Explorer", 
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Movie-Explorer", 
+  base: "./", 
 })
