590a4279e7d109867c30b83d6279779d23a1a189
Avukus <akashdeeprana04@gmail.com>
2025-12-19 18:58:12 +0530

    Changed default movie type

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://www.omdbapi.com/";
 
 export const getPopularMovies = async () => {
 
-  const response = await fetch(`${BASE_URL}?apikey=${API_KEY}&s=Batman`);
+  const response = await fetch(`${BASE_URL}?apikey=${API_KEY}&s=Action`);
   const data = await response.json();
   
   return data.Search || []; 
