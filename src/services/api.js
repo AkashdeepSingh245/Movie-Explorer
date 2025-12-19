e0a5a0d12bee91386ce1f665ce18317d12781b1e
Avukus <akashdeeprana04@gmail.com>
2025-12-19 17:49:57 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "c17eaf0";
+const BASE_URL = "http://www.omdbapi.com/";
+
+export const getPopularMovies = async () => {
+
+  const response = await fetch(`${BASE_URL}?apikey=${API_KEY}&s=Batman`);
+  const data = await response.json();
+  
+  return data.Search || []; 
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}?apikey=${API_KEY}&s=${encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  
+  return data.Search || [];
+};
